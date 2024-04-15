ddbf1c7d84df1de77720d6839800fd5a61ddce1f
Kelvin Lee <kelvin003@e.ntu.edu.sg>
2024-04-15 13:13:14 +0800

    add validate function to marmot-utils cli for users without access to modelstore

--- src/marmot_utils/cli.py
+++ src/marmot_utils/cli.py
@@ -12,4 +12,17 @@ def main():
 @click.argument("path_to_model", type=click.Path(exists=True))
 def upload(path_to_model: str) -> None:
     """Uploads a model to the model store"""
-    validate_model(path_to_model, print=click.echo)
+    if validate_model(path_to_model, print=click.echo):
+        upload_model(path_to_model, print=click.echo)
+
+    click.echo(f"==> Done!")
+
+
+@main.command()
+@click.argument("path_to_model", type=click.Path(exists=True))
+@click.option("--repo", type=str, default="")
+def validate(path_to_model: str, repo: str) -> None:
+    """Validate model"""
+    validate_model(
+        path_to_model, print=click.echo, local_repo=repo if repo != "" else None
+    )
