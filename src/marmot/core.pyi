31f0640c77ee6f3c375d1277a7aedf8a0a72a0d1
Kelvin Lee <kelvin003@e.ntu.edu.sg>
2024-04-24 11:30:46 +0800

    add stub files

--- src/marmot/core.pyi
+++ src/marmot/core.pyi
@@ -1,18 +1,26 @@
-from typing import Any
-
-from marmot.model.registration import ModelSpec as ModelSpec
+import abc
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Any, Generic, TypeVar
 
 class NotImplementedException(BaseException): ...
 
+@dataclass
 class ModelMetadata:
     id: str
-    category: str
+    def __init__(self, id) -> None: ...
+I = TypeVar('I')
+O = TypeVar('O')
 
-class Model:
-    metadata: ModelMetadata
-    spec: ModelSpec | None
-    def sample_input(self) -> None: ...
-    def get_output(self) -> None: ...
-    def __call__(self, *args: Any) -> Any: ...
+class Model(ABC, Generic[I, O], metaclass=abc.ABCMeta):
+    @property
+    @abstractmethod
+    def dummy_input(self) -> I: ...
+    @property
+    @abstractmethod
+    def dummy_output(self) -> O: ...
+    @abstractmethod
+    def get_output(self, *args: Any, **kwargs: Any) -> O: ...
     @classmethod
-    def register(cls) -> None: ...
+    def register_model(cls) -> None: ...
+    def __call__(self, *args: Any, **kwargs: Any) -> O: ...
